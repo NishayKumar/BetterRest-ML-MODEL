a8b4af9724002a70e2e214c06b66aca326facbf3
Nishaykumar <Nishaykumar24@gmail.com>
2023-08-31 22:56:15 +0530

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nishay Kumar on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
